42e8408a9887afb02a05decf7002e167a8519411
swapnil singh <68501882+swapnilsingh-hash@users.noreply.github.com>
2023-07-11 23:09:46 +0530

    Add files via upload

---
+++ Day16_Array.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.*;
+public class Day16_Array {
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int marks[]=new int[50];
+        int numbers[]= {1,2,3,4,5,6}; // automaticlly it has given size 3
+        int MoreNumbers[]={3,4,5,6,7};
+        String fruits[]={"Mango","Apple","Orange"};
+
+        // marks[0]=sc.nextInt();
+        // marks[1]=sc.nextInt();
+        // marks[2]=sc.nextInt();
+
+
+        // System.out.println("Phy : "+ marks[0]);
+        // System.out.println("che : "+ marks[1]);
+        // System.out.println("math : "+ marks[2]);
+
+        System.out.println("Size of Array: "+ marks.length);
+
+
+    }
+    
+}
