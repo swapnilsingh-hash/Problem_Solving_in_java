bfd8e11ed7d8f1393951e03a320bee6bf0696db4
swapnil singh <68501882+swapnilsingh-hash@users.noreply.github.com>
2023-07-19 00:17:51 +0530

    Add files via upload

---
+++ Day23_BubbleSort.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Day23_BubbleSort {
+
+    public static void bubbleSort(int arr[]){
+        // outer loop will count turns
+        for(int turn=0; turn < arr.length-1; turn++ ) {
+            for(int j=0;j<arr.length-1-turn; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp= arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1]= temp;
+                }
+
+            }
+
+        }
+    }
+
+    public static void PrintArray(int arr[]){
+        for(int i=0;i< arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        int arr[]={5,4,1,3,2};
+        bubbleSort(arr);
+        
+        PrintArray(arr);
+
+
+
+    }
+    
+}
